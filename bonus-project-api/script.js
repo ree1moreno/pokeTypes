6dd604e2397c2d7933c3910e000f9df88a77c898
Renato Moreno <renats.mc@gmail.com>
2021-09-28 17:12:31 -0300

    teste

--- bonus-project-api/script.js
+++ bonus-project-api/script.js
@@ -1,7 +1,56 @@
+const getButtonByType = document.getElementById('get-by-types');
+
 const getRandomNumber = (min, max) => {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min));
+}
+
+const getRandomPokemon = (arr) => {
+  const random = getRandomNumber(0, (arr.lenght-1))
+  return arr[random];
+}
+
+const eventListenerTypeButtons = () => {
+  const buttonType = document.getElementsByClassName('symbol');
+  const arrayButton = Array.from(buttonType);
+  
+  arrayButton.forEach((button) => {
+    button.addEventListener('click', async (event) => {
+      event.target.classList.toggle('selected');
+      const selectedClass = document.getElementsByClassName('selected');
+
+      if(selectedClass.length > 2) {
+        const arraySelected = Array.from(selectedClass);
+        arraySelected.forEach((e) => {
+        e.classList.remove('selected');
+        });
+      };
+    });
+  });
+};
+
+function filterPokemons() {
+  getButtonByType.addEventListener('click', async () => {
+    const selectedClass = document.getElementsByClassName('selected');
+    const arrFromSelected = Array.from(selectedClass);
+
+    const getID = arrFromSelected.map((e) => {
+        return e.id;
+    })
+    
+    const pokemons = await getPokemonByTypes(...getID);
+    const pokemon =  pokemons[getRandomNumber(0, pokemons.length)]
+    appendPokemon(await getPokemonByName(pokemon));
+  });
+};
+
+filterPokemons();
+
+eventListenerTypeButtons();
+
+function toTitleCase(word) {
+  return word[0].toUpperCase() + word.slice(1).toLowerCase();
 }
 
 const fetchPokemon = (name) => {
@@ -9,6 +58,7 @@ const fetchPokemon = (name) => {
   .then((response) => response.json())
   .then((data) => data.pokemon)
 }
+
 const getPokemonByType = async (type) => {
   const pokemonByType = fetchPokemon(type)
   .then((data) => data.map((e) => {
@@ -19,16 +69,18 @@ const getPokemonByType = async (type) => {
 }
 
 async function getPokemonByTypes (type1, type2) {
-const firstType = await getPokemonByType(type1);
-const secondType = await getPokemonByType(type2);
-return firstType.filter((pokeA) => {
+  const firstType = await getPokemonByType(type1);
+  if(!type2) {
+      return firstType;
+  }
+  const secondType = await getPokemonByType(type2);
+  return firstType.filter((pokeA) => {
   return secondType.find((pokeB) => pokeA === pokeB);
-})
+  })
 }
-// console.log(await getPokemonByTypes('ground', 'fire'));
 
 async function getPokemonByName(pokeName) {
-return fetch(`https://pokeapi.co/api/v2/pokemon/${pokeName}/`)
+  return fetch(`https://pokeapi.co/api/v2/pokemon/${pokeName}/`)
   .then((response) => response.json())
   .then((data) => {
     const { name, types, sprites} = data;
@@ -38,39 +90,41 @@ return fetch(`https://pokeapi.co/api/v2/pokemon/${pokeName}/`)
 console.log((await getPokemonByName('landorus-incarnate')));
 
 function appendPokemon (pokemon) {
-const pokeContent = document.createElement('section');
-const pokeName = document.createElement('h2');
-const pokeImage = document.createElement('img');
-const pokeTypes = document.createElement('div');
+  const getPokeSection = document.querySelector('.chosen-pokemon');
+  getPokeSection.innerHTML = '';
 
-pokeImage.src = `${(pokemon.sprites.other.dream_world.front_default) ? 
-  pokemon.sprites.other.dream_world.front_default : 
-  pokemon.sprites.other['official-artwork'].front_default}`;
-pokeImage.className = 'poke-sprite';
-pokeName.innerText = `Você escolheu o ${pokemon.name}! `;
-pokeName.className = 'poke-name';
-pokeContent.className = 'poke-section';
-pokeTypes.className = 'poke-types'; 
+  const pokeContent = document.createElement('section');
+  const pokeName = document.createElement('h2');
+  const pokeImage = document.createElement('img');
+  const pokeTypes = document.createElement('div');
 
-pokemon.types.forEach((element) => {
-  const span = document.createElement('span');
-  span.classList.add(`${element.type.name}`);
-  span.classList.add('type');
-  span.innerHTML = `${element.type.name}`
-  pokeTypes.appendChild(span);
-})
+  pokeImage.src = `${(pokemon.sprites.other.dream_world.front_default) ? 
+    pokemon.sprites.other.dream_world.front_default : 
+    pokemon.sprites.other['official-artwork'].front_default}`;
+  pokeImage.className = 'poke-sprite';
+  pokeName.innerText = `Você escolheu o ${toTitleCase(pokemon.name)}! `;
+  pokeName.className = 'poke-name';
+  pokeContent.className = 'poke-section';
+  pokeTypes.className = 'poke-types'; 
 
-pokeContent.appendChild(pokeName);
-pokeContent.appendChild(pokeImage);
-pokeContent.appendChild(pokeTypes);
-
-const getPokeSection = document.querySelector('.chosen-pokemon')
-
-getPokeSection.appendChild(pokeContent);
+  pokemon.types.forEach((element) => {
+    const span = document.createElement('span');
+    span.classList.add(`${element.type.name}`);
+    span.classList.add('type');
+    span.innerHTML = `${toTitleCase(element.type.name)}`
+    pokeTypes.appendChild(span);
+  })
+  
+  pokeContent.appendChild(pokeName);
+  pokeContent.appendChild(pokeImage);
+  pokeContent.appendChild(pokeTypes);
+  
+  getPokeSection.appendChild(pokeContent);
 }
 
 const getButton = document.querySelector('#find-pokemon');
+const getInput = document.querySelector('#pokemon-text');
 
 getButton.addEventListener('click', async () => {
-appendPokemon(await getPokemonByName('landorus-incarnate'));
+  appendPokemon(await getPokemonByName(`${getInput.value.toLowerCase()}`));
 })
