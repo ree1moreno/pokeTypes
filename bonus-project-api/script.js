d3f83cf67b223682f3cd770bffcdf31b700f3d13
Renato Moreno <renats.mc@gmail.com>
2021-09-28 12:01:40 -0300

    iniciando js

--- bonus-project-api/script.js
+++ bonus-project-api/script.js
@@ -1,1 +1,22 @@
-console.log('teste')
+import fetch from "node-fetch";
+const fetchPokemon = (name) => {
+    return fetch(`https://pokeapi.co/api/v2/type/${name}/`)
+    .then((response) => response.json())
+    .then((data) => data.pokemon)
+}
+const getPokemonByType = async (type) => {
+    const pokemonByType = fetchPokemon(type)
+    .then((data) => data.map((e) => {
+        return e.pokemon.name;
+    }));
+    // const pokemons = pokemonByType.map((e) => {
+    //     return e.pokemon.name;
+    // })
+    return pokemonByType;
+}
+const fire = await getPokemonByType('bug');
+const grass = await getPokemonByType('flying');
+const fireGrass = fire.filter((e) => {
+    e === grass.filter((j) => j === e);
+})
+console.log(fireGrass);
