cf5b14f415e38216ea81f2617f5f71ddccffee60
alexandrexpj <alexandrexpjr@hotmail.com>
2021-09-28 13:56:58 -0300

    append charizard

--- bonus-project-api/script.js
+++ bonus-project-api/script.js
@@ -1,1 +1,69 @@
-console.log('teste')
+// import fetch from "node-fetch";
+
+const fetchPokemon = (name) => {
+    return fetch(`https://pokeapi.co/api/v2/type/${name}/`)
+    .then((response) => response.json())
+    .then((data) => data.pokemon)
+}
+const getPokemonByType = async (type) => {
+    const pokemonByType = fetchPokemon(type)
+    .then((data) => data.map((e) => {
+        return e.pokemon.name;
+    }));
+
+    return pokemonByType;
+}
+
+async function getPokemonByTypes (type1, type2) {
+  const firstType = await getPokemonByType(type1);
+  const secondType = await getPokemonByType(type2);
+  return firstType.filter((pokeA) => {
+    return secondType.find((pokeB) => pokeA === pokeB);
+  })
+}
+// console.log(await getPokemonByTypes('ground', 'fire'));
+
+async function getPokemonByName(pokeName) {
+  return fetch(`https://pokeapi.co/api/v2/pokemon/${pokeName}/`)
+    .then((response) => response.json())
+    .then((data) => {
+      const { name, types, sprites} = data;
+      return { name, types, sprites }
+    })
+}
+console.log((await getPokemonByName('charizard')));
+
+function appendPokemon (pokemon) {
+  const pokeContent = document.createElement('section');
+  const pokeName = document.createElement('h2');
+  const pokeImage = document.createElement('img');
+  const pokeTypes = document.createElement('div');
+
+  pokeImage.src = pokemon.sprites.front_default;
+  pokeImage.className = 'poke-sprite';
+  pokeName.innerText = `Você escolheu o ${pokemon.name}`;
+  pokeName.className = 'poke-name';
+  pokeContent.className = 'poke-section';
+  pokeTypes.className = 'poke-types'; 
+
+  pokemon.types.forEach((element) => {
+    const span = document.createElement('span');
+    span.className = `${element.type.name}`
+    span.innerHTML = `${element.type.name}`
+    pokeTypes.appendChild(span);
+  })
+  
+  pokeContent.appendChild(pokeName);
+  pokeContent.appendChild(pokeImage);
+  pokeContent.appendChild(pokeTypes);
+
+  const getPokeSection = document.querySelector('.chosen-pokemon')
+  
+  getPokeSection.appendChild(pokeContent);
+}
+
+const getButton = document.querySelector('#find-pokemon');
+
+getButton.addEventListener('click', async () => {
+  appendPokemon(await getPokemonByName('charizard'));
+})
